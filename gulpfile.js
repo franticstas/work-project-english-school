35b33c464d8804385cfaf8d0e11ecadb254cbfb9
Stas <franticstas@gmail.com>
2019-11-06 16:05:59 +0300

    add header

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+"use strict";
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var plumber = require("gulp-plumber");
+var pug = require('gulp-pug');
+var notify = require("gulp-notify");
+
+gulp.task('css', function () {
+  return gulp.src('app/sass/**/*.scss')
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('pug', function() {
+  return gulp.src("app/pug/**/*.pug")
+    .pipe(plumber({
+      errorHandler: notify.onError()
+    }))
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest("app"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('serve', function() {
+
+  browserSync.init({
+    server: "app",
+    notify: false,
+  });
+
+   gulp.watch("app/sass/**/*.{scss,sass}", gulp.series("css"));
+   gulp.watch("app/pug/**/*.pug", gulp.series("pug"));
+
+});
+
+
+gulp.task("start", gulp.series("css", "pug", "serve"));
